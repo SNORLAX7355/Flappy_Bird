2830363dacc9a5a91531589e476b015f07e10bd9
Snorlax <philliot1304@gmail.com>
2022-05-21 14:57:55 -0700

    little edits

--- bird.py
+++ bird.py
@@ -23,41 +23,37 @@ class Bird:
         self.jump = False
         self.a = 0
         self.b = 0
-        self.m, self.m2 = 1, 1
         self.v, self.v2 = 8, 8
         self.fv = 1
 
     def update(self, dt):
+        """Update bird position"""
         if self.jump and self.rect.top > 0 and (self.a % 2 == 0):
-            F = (1/2) * self.m * (self.v**2)
+            F = (1/2) * (self.v**2)
             self.y -= F
             self.v -= 1
 
             self.b = 0
-            self.m2 = 1
             self.v2 = 8
 
             if self.v == 0:
                 self.jump = False
                 self.fv = 1
                 self.v = 8
-                self.m = 1
                 self.a = 0
 
         if self.jump and self.rect.top > 0 and (self.a % 2 == 1):
-            F = (1/2) * self.m2 * (self.v2**2)
+            F = (1/2) * (self.v2**2)
             self.y -= F
             self.v2 -= 1
 
             self.b = 0
-            self.m = 1
             self.v = 8
             
             if self.v2 == 0:
                 self.jump = False
                 self.fv = 1
                 self.v2 = 8
-                self.m2 = 1
                 self.a = 0
 
         if self.rect.top < 0:
@@ -82,7 +78,6 @@ class Bird:
         self.y = self.rect.y
         self.jump = False
         self.a = 0
-        self.m, self.m2 = 1, 1
         self.v, self.v2 = 8, 8
         self.fv = 1
 
